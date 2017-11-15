96594feaf8e13e81bb893cbd752bd2b09cd67210
rcatlord <henrypartridge@henrys-mbp.tht.local>
2017-11-15 13:06:40 +0000

    Made styling amendments to CSS

--- IMD_2015/app.R
+++ IMD_2015/app.R
@@ -1,4 +1,4 @@
-## GM Index of Multiple Deprivation ##
+## Indices of Multiple Deprivation (2015) ##
 
 library(shiny) ; library(tidyverse) ; library(sf) ; library(leaflet) ; library(htmltools)
 
@@ -6,7 +6,10 @@ library(shiny) ; library(tidyverse) ; library(sf) ; library(leaflet) ; library(h
 
 # IMD 2015 by LSOA (source: Department for Communities and Local Government)
 df <- read_csv("https://github.com/traffordDataLab/open_data/raw/master/imd_2015/IMD_2015_long.csv",
-               col_types = cols(lsoa11cd = col_factor(NULL), measure = col_factor(NULL), value = col_double(), index_domain = col_factor(NULL))) %>% 
+               col_types = cols(lsoa11cd = col_factor(NULL),
+                                measure = col_factor(NULL), 
+                                value = col_double(), 
+                                index_domain = col_factor(NULL))) %>% 
   spread(measure, value) %>% 
   rename(score = Score, rank = Rank, decile = Decile)
 
@@ -16,31 +19,37 @@ lsoa <- st_read("https://github.com/traffordDataLab/boundaries/raw/master/lsoa.g
 # Local Authority boundaries (source: ONS Open Geography Portal)
 la <- st_read("https://github.com/traffordDataLab/boundaries/raw/master/local_authorities.geojson")
 
-
 ui <- bootstrapPage(
   tags$head(includeCSS("styles_base.css"), includeCSS("styles_shiny.css"), includeCSS("styles_map.css"),
-            tags$style(
-    type = "text/css",
-    "html, body {width:100%;height:100%}")),
+            tags$style(type = "text/css", "html, body {width:100%;height:100%}")),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE, draggable = TRUE, height = "auto",
-                tags$h4("IMD (2015)"),
+                tags$h4("Indices of Multiple Deprivation"),
                 fluidRow(
                   tabsetPanel(
-                tabPanel("Controls", 
-                radioButtons(inputId = "domain",
-                             label = NULL,
-                             choices = c("Index of Multiple Deprivation", "Income", "Employment", "Education, Skills and Training", 
+                    tabPanel("Controls",
+                             radioButtons(inputId = "domain", label = NULL,
+                                          choices = c("Index of Multiple Deprivation", "Income", "Employment", "Education, Skills and Training", 
                                          "Health Deprivation and Disability", "Crime", "Barriers to Housing and Services", "Living Environment"),
-                             selected = "Index of Multiple Deprivation"),
-                hr(),
-                uiOutput("info")),
-                tabPanel("About",
-                         tags$p("This map shows the English Index of Multiple Deprivation for Greater Manchester neighbourhoods"),
-                         tags$br(),
-                         tags$p("Source:", tags$a(href="https://www.gov.uk/government/statistics/english-indices-of-deprivation-2015", "DCLG"),
-                                br(), tags$a(href="https://trafforddatalab.github.io/assets/theme/leaflet/script.R", "view code", target="_blank")))
-                ))))
+                                         selected = "Index of Multiple Deprivation"),
+                             hr(),
+                             uiOutput("info")),
+                    tabPanel("About",
+                             tags$br(),
+                             tags$p("This application visualises the English Indices of Deprivation 2015 for Lower-layer Super Output Areas (LSOA) within Greater Manchester. Each LSOA contains approximately 1,500 residents or 650 households."),
+                             tags$p("Each LSOA is ranked from 1 (most deprived) to 32,844 (least deprived) and assigned to a decile ranging from 1 (most deprived 10%) to 10 (least deprived 10%)."),
+                             tags$h4("Sources"),
+                             "Data: ", tags$a(href="https://www.gov.uk/government/statistics/english-indices-of-deprivation-2015", "DCLG,2015", target="_blank"),
+                             tags$br(),         
+                             "Boundary data: ", tags$a(href="http://geoportal.statistics.gov.uk/", "ONS Open Geography Portal", target="_blank"),
+                             tags$br(),
+                             tags$h4("Credits"),
+                             "This", tags$a(href="https://cran.r-project.org/web/packages/shiny/index.html", "Shiny", target="_blank"), "app uses the following R packages:",
+                             tags$a(href="https://cran.r-project.org/web/packages/tidyverse/index.html", "tidyverse,", target="_blank"),
+                             tags$a(href="https://cran.r-project.org/web/packages/sf/index.html", "sf,", target="_blank"),
+                             tags$a(href="https://cran.r-project.org/web/packages/leaflet/index.html", "leaflet,", target="_blank"),
+                             tags$a(href="https://cran.r-project.org/web/packages/htmltools/index.html", "htmltools.", target="_blank")
+                )))))
              
 
 server <- function(input, output, session) {
@@ -56,13 +65,13 @@ server <- function(input, output, session) {
 
   output$info <- renderUI({
     if (is.null(values$highlight)) {
-      return(tags$h4("Hover over an LSOA"))
+      return(tags$h4("Hover over an LSOA for details"))
     } else {
-      lsoaName <- filteredData()$lsoa11cd[values$highlight == lsoa$lsoa11cd]
+      lsoaCode <- filteredData()$lsoa11cd[values$highlight == lsoa$lsoa11cd]
       return(tags$div(
-        HTML(paste(tags$h4("LSOA: ", lsoaName))),
-        HTML(paste("in ", tags$span(filteredData()[filteredData()$lsoa11cd == lsoaName,]$wd16nm), " Ward, ",
-                   tags$span(filteredData()[filteredData()$lsoa11cd == lsoaName,]$lad16nm), sep = "")),
+        HTML(paste(tags$h4("LSOA: ", lsoaCode))),
+        HTML(paste("in ", tags$span(filteredData()[filteredData()$lsoa11cd == lsoaCode,]$wd16nm), " Ward, ",
+                   tags$span(filteredData()[filteredData()$lsoa11cd == lsoaCode,]$lad16nm), sep = "")),
         br(), br(),
         HTML("<table style='width: 100%'>
                <tr>
@@ -71,9 +80,9 @@ server <- function(input, output, session) {
                <th>Decile</th>
                </tr>
                <tr>
-               <td style='width: 33%'>", filteredData()[filteredData()$lsoa11cd == lsoaName,]$score, "</td>
-               <td style='width: 34%'>", formatC(filteredData()[filteredData()$lsoa11cd == lsoaName,]$rank, format="f", big.mark = ",", digits=0), "</td>
-               <td style='width: 33%'>", filteredData()[filteredData()$lsoa11cd == lsoaName,]$decile, "</td>
+               <td style='width: 33%'>", filteredData()[filteredData()$lsoa11cd == lsoaCode,]$score, "</td>
+               <td style='width: 34%'>", formatC(filteredData()[filteredData()$lsoa11cd == lsoaCode,]$rank, format="f", big.mark = ",", digits=0), "</td>
+               <td style='width: 33%'>", filteredData()[filteredData()$lsoa11cd == lsoaCode,]$decile, "</td>
                </tr>
                </table>")
       ))
@@ -97,7 +106,7 @@ server <- function(input, output, session) {
       addTiles(urlTemplate = "", 
                attribution = '<a href="https://www.ons.gov.uk/methodology/geography/licences">Contains OS data © Crown copyright and database right (2017)</a>',
                group = "No background") %>% 
-      setView(-2.28417866956407, 53.5151885751656, zoom =11) %>% 
+      setView(-2.28417866956407, 53.5151885751656, zoom = 11) %>% 
       addLayersControl(position = 'topleft',
                        baseGroups = c("CartoDB", "OpenStreetMap", "Satellite", "No background"),
                        options = layersControlOptions(collapsed = TRUE))
@@ -119,7 +128,7 @@ server <- function(input, output, session) {
                                                         "text-shadow" = "-1px -1px 10px #757575, 1px -1px 10px #757575, 1px 1px 10px #757575, -1px 1px 10px #757575"))) %>%
       addLegend(position = "bottomleft", 
                 colors = c("#A31A31", "#D23B33", "#EB6F4A", "#FCB562", "#F4D78D", "#D8E9EC", "#AAD1DE", "#75A8C8", "#4D77AE", "#353B91"),
-                title = "IMD Deciles (2015)",
+                title = "Deciles of deprivation",
                 labels = c("10% most deprived", "2","3","4","5","6","7","8","9", "10% least deprived"), opacity = 0.4) %>%
       addControl(html = html_logo, position = "bottomright")
   })
