8a51e5332c50ea31a1adca3696c3691263d73813
James Austin <james.austin@trafford.gov.uk>
2020-05-07 10:57:14 +0100

    Updated to use the mid-year 2018 dataset. Have created a new output rather than overwriting so that both can be referenced as required.

--- static/trafford_population.R
+++ static/trafford_population.R
@@ -4,7 +4,7 @@
 library(sf) ; library(tidyverse) ; library(ggplot2) ; library(ggspatial) ; library(shadowtext) ; library(viridis)
 
 # load geospatial data
-pop <- read_csv("https://www.traffordDataLab.io/open_data/mid-2017_population_estimates/mid-2017_population_estimates_ward.csv") %>% 
+pop <- read_csv("https://www.trafforddatalab.io/open_data/population/mid-2018_population_estimates_ward.csv") %>% 
   filter(gender == "Persons") %>% 
   select(area_code, all_ages)
 
@@ -17,7 +17,7 @@ ggplot() +
   geom_sf(data = wards, aes(fill = all_ages), alpha = 1, colour = "#FFFFFF",  size = 0.5) +
   geom_sf(data = localities, fill = NA, colour = "#212121",  size = 1) +
   geom_shadowtext(data = wards, aes(x = lon, y = lat, label = area_name), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 2.5, bg.colour = "#212121", nudge_y = 0.002) +
-  geom_shadowtext(data = localities, aes(x = lon, y = lat, label = area_code), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 4, bg.colour = "#212121", nudge_y = -0.002) +
+  geom_shadowtext(data = localities, aes(x = lon, y = lat, label = locality), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 4, bg.colour = "#212121", nudge_y = -0.002) +
   scale_fill_viridis(discrete = F,
                      name = "Persons", label = scales::comma,
                      direction = -1,
@@ -31,9 +31,9 @@ ggplot() +
                        label.hjust = 0.5)) +
   annotation_scale(location = "bl", style = "ticks", line_col = "#212121", text_col = "#212121") +
   annotation_north_arrow(height = unit(0.8, "cm"), width = unit(0.8, "cm"), location = "tr", which_north = "true") +
-  labs(title = "Trafford's resident population (2017)",
+  labs(title = "Trafford's resident population (2018)",
        subtitle = NULL,
-       caption = "Source: Mid-2017 population estimates, ONS | @traffordDataLab\n Contains Ordnance Survey data © Crown copyright and database right 2018",
+       caption = "Source: Mid-2018 population estimates, ONS | @traffordDataLab\n Contains Ordnance Survey data © Crown copyright and database right 2019",
        x = NULL, y = NULL) +
   coord_sf(crs = st_crs(4326), datum = NA) +
   theme_void(base_family = "Roboto") +
@@ -46,4 +46,4 @@ ggplot() +
         legend.position = c(0.18, 0.95))
 
 # write results
-ggsave("output/trafford_population.png", dpi = 300, scale = 1)
+ggsave("output/trafford_population_2018.png", dpi = 300, scale = 1)
