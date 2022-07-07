42dfe5ca2ab624fd0a498d603b168f65fa0752cc
IGRiosTC <iris.garciarios@trafford.gov.uk>
2022-07-07 14:53:22 +0100

    Remove polling districts and add labels to markers

--- trafford_polling_stations/trafford_new_wards.R
+++ trafford_polling_stations/trafford_new_wards.R
@@ -14,7 +14,7 @@ library(htmltools)
 
 lgbce_final_proposals <- st_read("data/Trafford_final_proposals.geojson") 
 
-polling_districts <- st_read("https://www.trafforddatalab.io/spatial_data/polling_districts/trafford_polling_districts.geojson")
+#polling_districts <- st_read("https://www.trafforddatalab.io/spatial_data/polling_districts/trafford_polling_districts.geojson")
 
 polling_stations_raw <- st_read("https://www.trafforddatalab.io/open_data/elections/polling_stations/trafford_polling_stations.geojson") 
 
@@ -43,29 +43,37 @@ map <- leaflet(height = "100%", width = "100%") %>%
            attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a> | <a href="https://www.ons.gov.uk/methodology/geography/licences">Contains OS data © Crown copyright and database right (2021)</a> | Source: <a href="https://democratic.trafford.gov.uk/mgMemberIndex.aspx?FN=WARD&VW=TABLE&PIC=1" target="_blank">Trafford Council</a> , <a href="https://www.lgbce.org.uk/all-reviews/north-west/greater-manchester/trafford" target="_blank">LGBCE</a>',
            options = tileOptions(minZoom = 12, maxZoom = 17), group = "Low Detail") %>%
   
-  addPolygons(data = lgbce_final_proposals, weight = 7, fillOpacity = 0, opacity = 1, color = "#212121", group = "LGBCE final") %>%
+  addPolygons(data = lgbce_final_proposals, weight = 5, fillOpacity = 0, opacity = 1, color = "#212121", group = "LGBCE final") %>%
   
   addLabelOnlyMarkers(data = centroids_LGBCE, lng = ~long, lat = ~lat, label = ~as.character(Name), 
                       labelOptions = labelOptions(noHide = T, textOnly = T, direction = "top",
                                                   style = list(
                                                     "color"="white",
-                                                    "text-shadow" = "-1px -1px 10px #757575, 1px -1px 10px #757575, 1px 1px 10px #757575, -1px 1px 10px #757575"))) %>%
+                                                    "text-shadow" = "-1px -1px 10px #555555, 1px -1px 10px #555555, 1px 1px 10px #555555, -1px 1px 10px #555555"))) %>%
   
-  addPolygons(data = polling_districts, weight = 3, fillOpacity = 0.0, opacity = 1, color = "#2A81CB", label = ~area_name, group = "Polling Districts",  highlightOptions = highlightOptions(color = "yellow", weight = 3, bringToFront = TRUE)) %>% ##2A81CB
+  #addPolygons(data = polling_districts, weight = 3, fillOpacity = 0.0, opacity = 1, color = "#2A81CB", label = ~area_name, group = "Polling Districts",  highlightOptions = highlightOptions(color = "yellow", weight = 3, bringToFront = TRUE)) %>% ##2A81CB
   
   addAwesomeMarkers(data = polling_stations, ~long, ~lat, icon=icon, group = "Polling Stations", popup = ~popup) %>%
+  
+  addLabelOnlyMarkers(data = polling_stations, lng = ~long, lat = ~lat, label = ~name, group = "Polling Stations Labels",
+                      labelOptions = labelOptions(noHide = TRUE, textOnly = FALSE, direction = "bottom"
+                                                  )) %>%
+  
+  groupOptions("Polling Stations Labels", zoomLevels = 15:17) %>%
+  
 
   addControl(paste0("<h1>Trafford new wards and polling stations</h1>"), position = 'topright',  className = "map-title") %>% 
-  addLayersControl(
-    overlayGroups = c("Polling Districts", "Polling Stations"),
-    options = layersControlOptions(collapsed = FALSE)
-   ) %>%
+  
+  # addLayersControl(
+  #   overlayGroups = c("Polling Stations"),
+  #   options = layersControlOptions(collapsed = FALSE)
+  #  ) %>%
   
   addControlGPS(options = gpsOptions(position = "topleft", activate = TRUE, 
                                      autoCenter = TRUE, maxZoom = 60, 
                                      setView = TRUE)) %>%
   
-  hideGroup("Polling Districts") %>%
+  #hideGroup("Polling Districts") %>%
 
     onRender(paste0("function(el, x) {$('head').append(","\'<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, viewport-fit=cover\">\'",");}")) %>%
     onRender(paste0("function(el, x) {$('head').append(","\'<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Open+Sans%7CRoboto\"/>\'",");}"))
