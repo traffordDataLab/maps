19e5b5df5a414701b1301d7d1c9cab8c027dda26
IGRiosTC <iris.garciarios@trafford.gov.uk>
2023-04-20 13:59:39 +0100

    Localities and Wards 2023 map

--- static/trafford_localities.R
+++ static/trafford_localities.R
@@ -1,23 +1,41 @@
 # Trafford localities #
 
-library(tidyverse) ; library(sf) ; library(ggplot2) ; library(ggspatial)
+library(tidyverse) ; library(sf)
+library(nngeo)
 
-sf <- st_read("https://www.trafforddatalab.io/spatial_data/council_defined/trafford_localities.geojson")
+wards <- st_read("https://www.trafforddatalab.io/spatial_data/ward/2023/trafford_ward_full_resolution.geojson") %>%
+  mutate(area_name = gsub(" Ward", "", area_name))
+
+localities <- wards %>% 
+  mutate(area_name = 
+           case_when(
+             area_name %in% c("Ashton upon Mersey", "Brooklands","Sale Central", "Sale Moor", "Manor") ~ "Central",
+             area_name %in% c("Old Trafford", "Gorse Hill & Cornbrook", "Lostock & Barton", "Longford", "Stretford & Humphrey Park") ~ "North",
+             area_name %in% c("Altrincham", "Bowdon", "Broadheath", "Hale Barns & Timperley South", "Hale", "Timperley Central", "Timperley North") ~ "South",
+             area_name %in% c("Bucklow-St Martins", "Davyhulme", "Urmston", "Flixton", "Urmston") ~ "West"))  %>%
+  group_by(area_name) %>% 
+  summarise() %>%
+  mutate(lon = map_dbl(geometry, ~st_centroid(.x)[[1]]),
+         lat = map_dbl(geometry, ~st_centroid(.x)[[2]])) %>%
+  st_remove_holes()
 
 ggplot() +
-  geom_sf(data = sf, fill = "#CCE3AA", alpha = 1, colour = "#FFFFFF",  size = 2) +
-  geom_text(data = sf, aes(x = lon, y = lat, label = area_name), colour = "#FFFFFF", size = 4, fontface = "bold") +
-  annotation_scale(location = "bl", style = "ticks", line_col = "#212121", text_col = "#212121") +
-  annotation_north_arrow(height = unit(0.8, "cm"), width = unit(0.8, "cm"), location = "tr", which_north = "true") +
-  labs(title = "Trafford's localities",
-       subtitle = NULL,
-       caption = "Contains OS data © Crown copyright and database right (2018) | @traffordDataLab",
-       x = NULL, y = NULL) +
-  coord_sf(crs = st_crs(4326), datum = NA) +
-  theme_void(base_family = "Roboto") +
-  theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
-        text = element_text(colour = "#212121"),
-        plot.title = element_text(size = 18, face = "bold", colour = "#757575", margin = margin(t = 15), vjust = 4),
-        plot.caption = element_text(size = 10, colour = "#212121", margin = margin(b = 15), vjust = -4))
-
-ggsave("trafford_localities.png", dpi = 300, scale = 1)
+  geom_sf(data = wards, fill = "#DDDDCC", colour = "#ffffff" ,alpha = 1, size = 0.7) +
+  geom_text(data = wards,
+            aes(x = lon, y = lat, label = str_wrap(area_name, width = 15), group = area_name),
+            color = "#212121", size = 3, fontface = "bold",
+            box.padding = 0, point.padding = 0) +
+  geom_sf(data = localities, fill = "transparent", colour = "#757575", size = 1.4) +
+  geom_text(data = localities, mapping = aes(x = lon, y = lat, label = area_name), colour = "#757575", fontface = "bold", size = 5) +
+  geom_text(data = wards,
+            aes(x = lon, y = lat, label = str_wrap(area_name, width = 15), group = area_name),
+            color = "#212121", size = 3, fontface = "bold",
+            box.padding = 0, point.padding = 0) +
+  labs(x = NULL, y = NULL, title = "Trafford's 2023 localities and wards",
+       caption = "Contains OS data © Crown copyright and database right (2023). Source: Trafford Council\n@traffordDataLab") +
+  coord_sf(datum = NA) +
+  theme_void() +
+  theme(plot.title = element_text(size = 18),
+        axis.text = element_blank(),
+        legend.position = "none",
+        plot.margin = unit(c(0,0,0,0), "cm"))
