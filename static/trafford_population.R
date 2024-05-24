16d06776823d5024fa4237465230c52e9552b9b6
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-05-24 17:43:43 +0100

    Trafford's wards population mid 2022

--- static/trafford_population.R
+++ static/trafford_population.R
@@ -1,37 +1,49 @@
-## Trafford's population ##
+## Trafford's population map 2022##
 
 # load libraries ---------------------------------------------------------------
-library(sf) ; library(tidyverse) ; library(ggplot2) ; library(ggspatial) ; library(shadowtext) ; library(viridis)
+library(httr) ; library(readxl) ; library(tidyverse) ; library(sf) ; library(tidyverse) ; library(ggplot2) ; library(ggspatial) ; library(shadowtext) ; library(viridis)
 
-# load mid-2020 population estimates
+# load mid-2022 population estimates
 
-# Source: Nomis
-# URL: https://www.nomisweb.co.uk/datasets/pestsyoaoa
-pop <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2010_1.data.csv?geography=1656750701...1656750715,1656750717,1656750716,1656750718...1656750721&date=latest&gender=0&c_age=200&measures=20100&select=date_name,geography_name,geography_code,gender_name,c_age_name,measures_name,obs_value,obs_status_name") %>% 
-  select(area_code = GEOGRAPHY_CODE, n = OBS_VALUE)
+# Source: Mid-year estimates 2022. Ward-level population estimates (official statistics in development
+# URL: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental
+# Licence: Open Government Licence
+
+tmp <- tempfile(fileext = ".xlsx")
+
+
+GET(url = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental/mid2021andmid2022/sapewardstablefinal.xlsx",
+    write_disk(tmp))
+
+df <- read_xlsx(tmp, sheet = 8, skip = 3) %>%
+  filter(`LAD 2023 Code` == "E08000009") %>%
+  select(area_code = `Ward 2023 Code`, n = Total)
 
 # load geospatial data ---------------------------------------------------------
 
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/wards-december-2019-boundaries-ew-bgc
-lookup <- read_csv("https://opendata.arcgis.com/datasets/e169bb50944747cd83dcfb4dd66555b1_0.csv") %>% 
-  filter(LAD19NM == "Trafford") %>% 
-  pull(WD19CD)
 
-wards <- st_read(paste0("https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Wards_December_2019_Boundaries_EW_BGC/MapServer/0/query?where=", 
-                        URLencode(paste0("wd19cd IN (", paste(shQuote(lookup), collapse = ", "), ")")), 
-                        "&outFields=wd19cd,wd19nm,long,lat&outSR=4326&f=geojson")) %>% 
-  select(area_code = wd19cd, area_name = wd19nm, lon = long, lat) %>% 
-  left_join(., pop, by = "area_code")
+codes <- fromJSON(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/WD23_LAD23_UK_LU_DEC/FeatureServer/0/query?where=LAD23NM%20%3D%20'", URLencode(toupper("Trafford"), reserved = TRUE), "'&outFields=WD23CD,WD23NM,LAD23CD,LAD23NM&outSR=4326&f=json"), flatten = TRUE) %>% 
+  pluck("features") %>% 
+  as_tibble() %>% 
+  distinct(attributes.WD23CD) %>% 
+  pull(attributes.WD23CD) 
 
-localities <- st_read("https://www.traffordDataLab.io/spatial_data/council_defined/trafford_localities.geojson")
+wards <- st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Wards_December_2023_Boundaries_UK_BFE/FeatureServer/0/query?where=", 
+                        URLencode(paste0("WD23CD IN (", paste(shQuote(codes), collapse = ", "), ")")), 
+                        "&outFields=WD23CD,WD23NM,LONG,LAT&outSR=4326&f=json")) %>%
+  select(area_code = WD23CD, area_name = WD23NM, lon = LONG, lat = LAT) %>%
+  left_join(df, by = "area_code") %>%
+  mutate(label = paste0(str_wrap(area_name, width = 15),"\n",format(n, big.mark = ",")))
+
+localities <- st_read("https://www.traffordDataLab.io/spatial_data/council_defined/2023/trafford_localities_full_resolution.geojson")
 
 # plot map ---------------------------------------------------------------------
 ggplot() +
-  geom_sf(data = wards, aes(fill = n), alpha = 1, colour = "#FFFFFF",  size = 0.5) +
-  geom_sf(data = localities, fill = NA, colour = "#212121",  size = 1) +
-  geom_shadowtext(data = wards, aes(x = lon, y = lat, label = area_name), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 2.5, bg.colour = "#212121", nudge_y = 0.002) +
-  geom_shadowtext(data = localities, aes(x = lon, y = lat, label = locality), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 4, bg.colour = "#212121", nudge_y = -0.002) +
+  geom_sf(data = wards, aes(fill = n), alpha = 1, colour = "#FFFFFF",  linewidth = 0.5) +
+  geom_sf(data = localities, fill = NA, colour = "#212121",  linewidth = 1) +
+  geom_shadowtext(data = wards, aes(label = label, geometry = geometry), stat = "sf_coordinates", colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 2.5, bg.colour = "#212121", nudge_y = 0.002) +
+  geom_shadowtext(data = localities, aes(x = lon, y = lat, label = area_name), colour = "#FFFFFF", family = "Open Sans", fontface = "bold", size = 4, bg.colour = "#212121", nudge_y = -0.002) +
   scale_fill_viridis(discrete = F,
                      name = "Persons", label = scales::comma,
                      direction = -1,
@@ -45,9 +57,9 @@ ggplot() +
                        label.hjust = 0.5)) +
   annotation_scale(location = "bl", style = "ticks", line_col = "#212121", text_col = "#212121") +
   annotation_north_arrow(height = unit(0.8, "cm"), width = unit(0.8, "cm"), location = "tr", which_north = "true") +
-  labs(title = "Trafford's resident population (2020)",
+  labs(title = "Trafford's resident population (2022)",
        subtitle = NULL,
-       caption = "Source: Mid-2020 population estimates, ONS | @traffordDataLab\n Contains Ordnance Survey data © Crown copyright and database right 2021",
+       caption = "Source: Mid-2020 population estimates, ONS | @traffordDataLab\n Contains Ordnance Survey data © Crown copyright and database right 2024",
        x = NULL, y = NULL) +
   coord_sf(crs = st_crs(4326), datum = NA) +
   theme_void(base_family = "Roboto") +
@@ -60,4 +72,4 @@ ggplot() +
         legend.position = c(0.18, 0.95))
 
 # write results ----------------------------------------------------------------
-ggsave("output/trafford_population_2020.png", dpi = 300, scale = 1)
+ggsave("output/trafford_population_2022.png", dpi = 300, scale = 1)
